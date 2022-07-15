fb1a54e9ed949687f8331b24c9489d52a5d8558b
Liyu <wangliyu@huoxing24.com>
2022-07-15 17:35:51 +0800

    del zero v4

--- src/contracts/abi/index.ts
+++ src/contracts/abi/index.ts
@@ -1,7 +1,6 @@
 import ElementEx from './zeroEx/ElementEx.json'
 import Exchange from './zeroEx/Exchange.json'
 import Forwarder from './zeroEx/Forwarder.json'
-import IZeroExV4 from './zeroEx/IZeroExV4.json'
 import IZeroExV3 from './zeroEx/ZeroExV3MarketProxy.json'
 import ExSwap from './ExSwap.json'
 
@@ -16,7 +15,6 @@ export const ContractABI = {
   elementEx: ElementEx as AbiInfo,
   exchange: Exchange as AbiInfo,
   forwarder: Forwarder as AbiInfo,
-  zeroExV4: IZeroExV4 as AbiInfo,
   swapEx: ExSwap as AbiInfo,
   zeroExV3: IZeroExV3 as AbiInfo
 }
